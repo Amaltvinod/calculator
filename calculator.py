cd35599fa66bdeebe278c9f14719877deef12690
Amaltvinod <amaltvinod5@gmail.com>
2021-08-20 15:01:50 +0530

    code changes

--- calculator.py
+++ calculator.py
@@ -3,20 +3,27 @@ from tkinter import *
 root=Tk()
 root.title("calculator")
 root.geometry("600x600")
-space=Entry(root,width=40)
-space.grid(row=0, column=0, columnspan=3, pady=20)
-def hello( number):
-    space.insert(0,number)
-button1=Button(root,text="0", command= lambda: hello(0), padx=32, pady=20)
-button2=Button(root,text="1", command= lambda: hello(1), padx=32, pady=20)
-button3=Button(root,text="2", command= lambda: hello(2), padx=32, pady=20)
-button4=Button(root,text="3", command= lambda: hello(3), padx=32, pady=20)
-button5=Button(root,text="4", command= lambda: hello(4), padx=32, pady=20)
-button6=Button(root,text="5", command= lambda: hello(5), padx=32, pady=20)
-button7=Button(root,text="6", command= lambda: hello(6), padx=32, pady=20)
-button8=Button(root,text="7", command= lambda: hello(7), padx=32, pady=20)
-button9=Button(root,text="8", command= lambda: hello(8), padx=32, pady=20)
-button10=Button(root,text="9", command= lambda: hello(9),padx=32, pady=20)
+input=Entry(root,width=40)
+input.grid(row=0, column=0, columnspan=3, pady=20)
+def get_num(number):
+    global previous
+    previous=input.get()
+    input.delete(0,END)
+    input.insert(0,str(previous)+str(number))
+
+
+def addition():
+    pass
+button1=Button(root,text="0", command= lambda: get_num(0), padx=32, pady=20)
+button2=Button(root,text="1", command= lambda: get_num(1), padx=32, pady=20)
+button3=Button(root,text="2", command= lambda: get_num(2), padx=32, pady=20)
+button4=Button(root,text="3", command= lambda: get_num(3), padx=32, pady=20)
+button5=Button(root,text="4", command= lambda: get_num(4), padx=32, pady=20)
+button6=Button(root,text="5", command= lambda: get_num(5), padx=32, pady=20)
+button7=Button(root,text="6", command= lambda: get_num(6), padx=32, pady=20)
+button8=Button(root,text="7", command= lambda: get_num(7), padx=32, pady=20)
+button9=Button(root,text="8", command= lambda: get_num(8), padx=32, pady=20)
+button10=Button(root,text="9", command= lambda: get_num(9),padx=32, pady=20)
 button11=Button(root,text="+",padx=32, pady=20)
 button12=Button(root,text="-", padx=32, pady=20)
 button13=Button(root,text="*", padx=32, pady=20)
